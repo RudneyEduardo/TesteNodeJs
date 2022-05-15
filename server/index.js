fbd20a438972091c845c6d3c27cd612e52906ccc
Rudney eduardo souza vieira <50462041+RudneyEduardo@users.noreply.github.com>
2022-05-15 17:25:53 -0300

    Adding Swagger

--- server/index.js
+++ server/index.js
@@ -2,10 +2,36 @@ import 'dotenv/config'
 import express from 'express'
 import cep from '../routes/cep.js'
 import auth from '../routes/auth.js'
+import swaggerUiExpress from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
+
 
 const app = express()
 
 app.use(express.json())
+
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:3000",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+
+//*Swagger
+app.use("/api-docs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 
 //*Auth route
--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken';
 
-
 /**
  * AuthenticateToken
  * * This Function takes the req, res and callback function
